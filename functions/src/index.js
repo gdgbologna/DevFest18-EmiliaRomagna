00def1a28ef27af9356f51af32534650a0615ffe
Devid Farinelli <devid.farinelli@gmail.com>
2018-10-12 19:17:30 +0200

    NEW LAYOUT, Deleted cfuncs, bell, fixed page descriptions/footer/ticket

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,20 +1,20 @@
-import * as functions from 'firebase-functions';
-import admin from 'firebase-admin';
+// import * as functions from 'firebase-functions';
+// import admin from 'firebase-admin';
 
-import saveUserData from './save-user-data';
-import sendGeneralNotification from './notifications';
-import scheduleNotifications from './schedule-notifications';
-import optimizeImages from './optimize-images';
-import mailchimpSubscribe from './mailchimp-subscribe';
-import prerender from './prerender';
+// // import saveUserData from './save-user-data';
+// // import sendGeneralNotification from './notifications';
+// // import scheduleNotifications from './schedule-notifications';
+// // import optimizeImages from './optimize-images';
+// // import mailchimpSubscribe from './mailchimp-subscribe';
+// // import prerender from './prerender';
 
-admin.initializeApp();
+// admin.initializeApp();
 
-export {
-  saveUserData,
-  sendGeneralNotification,
-  scheduleNotifications,
-  optimizeImages,
-  mailchimpSubscribe,
-  prerender,
-}
+// export {
+//   saveUserData,
+//   sendGeneralNotification,
+//   scheduleNotifications,
+//   optimizeImages,
+//   mailchimpSubscribe,
+//   prerender,
+// }
